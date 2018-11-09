f07b4c9765010241224d31496fe0aab4aabce47f
Seongwoon <seanjeong3@gmail.com>
2018-11-08 16:47:29 -0800

    install

--- setup.py
+++ setup.py
@@ -119,9 +119,11 @@ def install_cassandra(param):
 
 ##### Node.js Setup #####
 def install_nodejs():
-	os.system('curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -')
+	os.system('curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -')
 	os.system('sudo apt-get install -y nodejs')
 	os.system('sudo apt install npm')
+
+	print("steel working!")
 
 
 
